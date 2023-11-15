41f5684ab84a705534f769a59a488a62c41f1985
killeruzumaki25 <150746435+killeruzumaki25@users.noreply.github.com>
2023-11-15 08:23:16 -0800

    Update style.js

--- style.js
+++ style.js
@@ -5,4 +5,14 @@ body{
   background-color:red;
   display:flex;
   flex-direction:column;
+  position:fixed;
+}
+.todoInput{
+  width:100%;
+  height:5vh;
+}
+
+.saveBtn{
+  height:5vh;
+  width:20%;
 }
