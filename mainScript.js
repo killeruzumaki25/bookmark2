81179ee75575563647925394f4084415eb6f817a
killeruzumaki25 <150746435+killeruzumaki25@users.noreply.github.com>
2023-11-14 18:03:30 -0800

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -1,9 +1,9 @@
-var oldBody = document.querySelector("body");
+let oldBody = document.querySelector("body");
 
-var newHead = document.createElement('head');
+let newHead = document.createElement('head');
 oldBody.appendChild(newHead);
 
-var icons = document.createElement('link');
+let icons = document.createElement('link');
 icons.innerHTML="<link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0' />";
 newHead.appendChild(icons);
 
@@ -11,6 +11,16 @@ const con = document.createElement('div');
 con.classList.add('con');
 oldBody.appendChild(con);
 
+let inp = document.getElementById('input')
+function btn(){
+  if(inp.value){
+    localStorage.setItem("todo",inp.value)
+    alert("done")
+  }
+}
 
+setTimeout(()=>{
+  alert(localStorage.getItem("todo"));
+},5000)
 
 
